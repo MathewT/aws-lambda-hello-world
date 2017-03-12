c60d6a0c2a838951db52f08f8a1c2e905c121e02
Mathew Thomas <mathew.thomas@longdivision.com>
2017-03-12 14:47:47 -0400

    hello world.
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# AWS Lambda Hello World
+
+## Node.js Setup on Ubuntu
+
+
+## Deployment to AWS
+
+```bash
+npm run deploy
+
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+var AWS = require('aws-sdk');
+
+
+exports.handler = function(event, context, callback){
+    console.log('Welcome to AWS Lambda.');
+};
