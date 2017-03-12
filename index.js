4e54e67c646be4af5b2442a8eb68bc5d5e5aeb83
Mathew Thomas <mathew.thomas@longdivision.com>
2017-03-12 15:43:18 -0400

    --allow-empty

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var AWS = require('aws-sdk');
 
 
 exports.handler = function(event, context, callback){
-    console.log('Welcome to AWS Lambda.');
+    console.log('Welcome to my hello world lambda.');
 };
